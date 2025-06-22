d63c3f8391406379a66b26888b4c647a89fb5aac
MHmastercode123 <laapaos886@gmail.com>
2025-06-22 01:17:07 -0600

    commit rony

---
+++ DTOs/VerificacionVendedorDTO/VerificacioVendedorDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProyectoDeCursoE_Commerce.DTOs.VerificacionVendedorDTO
+{
+    public class VerificacioVendedorDTO
+    {
+        public int VendedorId { get; set; }
+        public int UsuarioId { get; set; }
+        public string DireccionOrigen { get; set; }
+    }
+}
---
+++ Migrations/20250621231009_ApiComerce.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiProyectoDeCursoE_Commerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApiComerce : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
