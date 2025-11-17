058aa5b248ea8646a19ffe5f7251ab18a83c7d47
MHmastercode123 <laapaos886@gmail.com>
2025-11-16 21:09:17 -0600

    cambios en Forms , en la y ui del sistema y resurses de fotos

---
+++ FormApiE-Commerce/Views/FormInicio.Designer.cs
@@ -0,0 +1,397 @@
+﻿namespace FormApiE_Commerce
+{
+    partial class FormInicio
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormInicio));
+            toolTip1 = new ToolTip(components);
+            panel2 = new Panel();
+            cuiButton3 = new CuoreUI.Controls.cuiButton();
+            cuiLabel2 = new CuoreUI.Controls.cuiLabel();
+            cuiGroupBox2 = new CuoreUI.Controls.cuiGroupBox();
+            cuiTextBox2 = new CuoreUI.Controls.cuiTextBox();
+            cuiGroupBox1 = new CuoreUI.Controls.cuiGroupBox();
+            cuiTextBox1 = new CuoreUI.Controls.cuiTextBox();
+            cuiPictureBox2 = new CuoreUI.Controls.cuiPictureBox();
+            cuiLabel1 = new CuoreUI.Controls.cuiLabel();
+            cuiButton2 = new CuoreUI.Controls.cuiButton();
+            cuiButton1 = new CuoreUI.Controls.cuiButton();
+            panel3 = new Panel();
+            cuiPictureBox1 = new CuoreUI.Controls.cuiPictureBox();
+            cuiFileDropper1 = new CuoreUI.Controls.cuiFileDropper();
+            panel2.SuspendLayout();
+            cuiGroupBox2.SuspendLayout();
+            cuiGroupBox1.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(cuiButton3);
+            panel2.Controls.Add(cuiLabel2);
+            panel2.Controls.Add(cuiGroupBox2);
+            panel2.Controls.Add(cuiGroupBox1);
+            panel2.Controls.Add(cuiPictureBox2);
+            panel2.Controls.Add(cuiLabel1);
+            panel2.Controls.Add(cuiButton2);
+            panel2.Controls.Add(cuiButton1);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(444, 580);
+            panel2.TabIndex = 1;
+            // 
+            // cuiButton3
+            // 
+            cuiButton3.BackColor = SystemColors.Control;
+            cuiButton3.CheckButton = false;
+            cuiButton3.Checked = false;
+            cuiButton3.CheckedBackground = Color.FromArgb(255, 106, 0);
+            cuiButton3.CheckedForeColor = Color.White;
+            cuiButton3.CheckedImageTint = Color.White;
+            cuiButton3.CheckedOutline = Color.FromArgb(255, 106, 0);
+            cuiButton3.Content = "Developers";
+            cuiButton3.DialogResult = DialogResult.None;
+            cuiButton3.Font = new Font("Garamond", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cuiButton3.ForeColor = Color.Black;
+            cuiButton3.HoverBackground = Color.DarkGray;
+            cuiButton3.HoverForeColor = Color.Black;
+            cuiButton3.HoverImageTint = Color.White;
+            cuiButton3.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            cuiButton3.Image = null;
+            cuiButton3.ImageAutoCenter = true;
+            cuiButton3.ImageExpand = new Point(0, 0);
+            cuiButton3.ImageOffset = new Point(0, 0);
+            cuiButton3.Location = new Point(235, 535);
+            cuiButton3.Name = "cuiButton3";
+            cuiButton3.NormalBackground = Color.DarkGray;
+            cuiButton3.NormalForeColor = Color.Black;
+            cuiButton3.NormalImageTint = Color.White;
+            cuiButton3.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton3.OutlineThickness = 1F;
+            cuiButton3.PressedBackground = Color.WhiteSmoke;
+            cuiButton3.PressedForeColor = Color.FromArgb(32, 32, 32);
+            cuiButton3.PressedImageTint = Color.White;
+            cuiButton3.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton3.Rounding = new Padding(8);
+            cuiButton3.Size = new Size(191, 33);
+            cuiButton3.TabIndex = 7;
+            cuiButton3.TextAlignment = StringAlignment.Center;
+            cuiButton3.TextOffset = new Point(0, 0);
+            // 
+            // cuiLabel2
+            // 
+            cuiLabel2.Content = "Contactanos\\ aqui:";
+            cuiLabel2.Font = new Font("Garamond", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cuiLabel2.HorizontalAlignment = StringAlignment.Center;
+            cuiLabel2.Location = new Point(20, 541);
+            cuiLabel2.Margin = new Padding(4, 5, 4, 5);
+            cuiLabel2.Name = "cuiLabel2";
+            cuiLabel2.Size = new Size(208, 25);
+            cuiLabel2.TabIndex = 6;
+            cuiLabel2.VerticalAlignment = StringAlignment.Near;
+            // 
+            // cuiGroupBox2
+            // 
+            cuiGroupBox2.BorderColor = Color.DarkGoldenrod;
+            cuiGroupBox2.Content = "Contraseña";
+            cuiGroupBox2.Controls.Add(cuiTextBox2);
+            cuiGroupBox2.Font = new Font("Garamond", 13.2000008F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cuiGroupBox2.ForeColor = Color.DarkOliveGreen;
+            cuiGroupBox2.Location = new Point(29, 208);
+            cuiGroupBox2.Name = "cuiGroupBox2";
+            cuiGroupBox2.Padding = new Padding(10, 33, 10, 15);
+            cuiGroupBox2.Rounding = new Padding(10, 10, 10, 15);
+            cuiGroupBox2.Size = new Size(356, 72);
+            cuiGroupBox2.TabIndex = 4;
+            // 
+            // cuiTextBox2
+            // 
+            cuiTextBox2.BackgroundColor = Color.WhiteSmoke;
+            cuiTextBox2.Content = "";
+            cuiTextBox2.FocusBackgroundColor = Color.White;
+            cuiTextBox2.FocusImageTint = Color.White;
+            cuiTextBox2.FocusOutlineColor = Color.FromArgb(255, 106, 0);
+            cuiTextBox2.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cuiTextBox2.ForeColor = Color.Gray;
+            cuiTextBox2.Image = null;
+            cuiTextBox2.ImageExpand = new Point(0, 0);
+            cuiTextBox2.ImageOffset = new Point(0, 0);
+            cuiTextBox2.Location = new Point(11, 26);
+            cuiTextBox2.Margin = new Padding(4);
+            cuiTextBox2.Multiline = false;
+            cuiTextBox2.Name = "cuiTextBox2";
+            cuiTextBox2.NormalImageTint = Color.White;
+            cuiTextBox2.OutlineColor = Color.FromArgb(128, 128, 128, 128);
+            cuiTextBox2.Padding = new Padding(18, 8, 18, 0);
+            cuiTextBox2.PasswordChar = false;
+            cuiTextBox2.PlaceholderColor = SystemColors.WindowText;
+            cuiTextBox2.PlaceholderText = "";
+            cuiTextBox2.Rounding = new Padding(8);
+            cuiTextBox2.Size = new Size(332, 35);
+            cuiTextBox2.TabIndex = 1;
+            cuiTextBox2.TextOffset = new Size(0, 0);
+            cuiTextBox2.UnderlinedStyle = true;
+            // 
+            // cuiGroupBox1
+            // 
+            cuiGroupBox1.BorderColor = Color.DarkGoldenrod;
+            cuiGroupBox1.Content = "Email";
+            cuiGroupBox1.Controls.Add(cuiTextBox1);
+            cuiGroupBox1.Font = new Font("Garamond", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cuiGroupBox1.ForeColor = Color.DarkOliveGreen;
+            cuiGroupBox1.Location = new Point(29, 120);
+            cuiGroupBox1.Name = "cuiGroupBox1";
+            cuiGroupBox1.Padding = new Padding(10, 34, 10, 15);
+            cuiGroupBox1.Rounding = new Padding(10, 10, 10, 15);
+            cuiGroupBox1.Size = new Size(356, 69);
+            cuiGroupBox1.TabIndex = 3;
+            // 
+            // cuiTextBox1
+            // 
+            cuiTextBox1.BackgroundColor = Color.WhiteSmoke;
+            cuiTextBox1.Content = "";
+            cuiTextBox1.FocusBackgroundColor = Color.White;
+            cuiTextBox1.FocusImageTint = Color.White;
+            cuiTextBox1.FocusOutlineColor = Color.FromArgb(255, 106, 0);
+            cuiTextBox1.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cuiTextBox1.ForeColor = Color.Gray;
+            cuiTextBox1.Image = null;
+            cuiTextBox1.ImageExpand = new Point(0, 0);
+            cuiTextBox1.ImageOffset = new Point(0, 0);
+            cuiTextBox1.Location = new Point(10, 25);
+            cuiTextBox1.Margin = new Padding(4);
+            cuiTextBox1.Multiline = false;
+            cuiTextBox1.Name = "cuiTextBox1";
+            cuiTextBox1.NormalImageTint = Color.White;
+            cuiTextBox1.OutlineColor = Color.FromArgb(128, 128, 128, 128);
+            cuiTextBox1.Padding = new Padding(18, 8, 18, 0);
+            cuiTextBox1.PasswordChar = false;
+            cuiTextBox1.PlaceholderColor = SystemColors.WindowText;
+            cuiTextBox1.PlaceholderText = "";
+            cuiTextBox1.Rounding = new Padding(8);
+            cuiTextBox1.Size = new Size(332, 35);
+            cuiTextBox1.TabIndex = 0;
+            cuiTextBox1.TextOffset = new Size(0, 0);
+            cuiTextBox1.UnderlinedStyle = true;
+            // 
+            // cuiPictureBox2
+            // 
+            cuiPictureBox2.Content = Properties.Resources.LRR_removebg_preview;
+            cuiPictureBox2.ImageTint = Color.White;
+            cuiPictureBox2.Location = new Point(13, -40);
+            cuiPictureBox2.Margin = new Padding(4, 5, 4, 5);
+            cuiPictureBox2.Name = "cuiPictureBox2";
+            cuiPictureBox2.OutlineThickness = 1F;
+            cuiPictureBox2.PanelOutlineColor = Color.Empty;
+            cuiPictureBox2.Rotation = 0;
+            cuiPictureBox2.Rounding = new Padding(8);
+            cuiPictureBox2.Size = new Size(227, 214);
+            cuiPictureBox2.TabIndex = 5;
+            // 
+            // cuiLabel1
+            // 
+            cuiLabel1.Content = "Todavia\\ no\\ tienes\\ una\\ cuenta\\?";
+            cuiLabel1.Font = new Font("Garamond", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            cuiLabel1.HorizontalAlignment = StringAlignment.Center;
+            cuiLabel1.Location = new Point(35, 428);
+            cuiLabel1.Margin = new Padding(4, 5, 4, 5);
+            cuiLabel1.Name = "cuiLabel1";
+            cuiLabel1.Size = new Size(350, 21);
+            cuiLabel1.TabIndex = 2;
+            cuiLabel1.VerticalAlignment = StringAlignment.Near;
+            // 
+            // cuiButton2
+            // 
+            cuiButton2.CheckButton = false;
+            cuiButton2.Checked = false;
+            cuiButton2.CheckedBackground = Color.FromArgb(255, 106, 0);
+            cuiButton2.CheckedForeColor = Color.White;
+            cuiButton2.CheckedImageTint = Color.White;
+            cuiButton2.CheckedOutline = Color.FromArgb(255, 106, 0);
+            cuiButton2.Content = "Registrarse";
+            cuiButton2.DialogResult = DialogResult.None;
+            cuiButton2.Font = new Font("Garamond", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cuiButton2.ForeColor = Color.Black;
+            cuiButton2.HoverBackground = Color.White;
+            cuiButton2.HoverForeColor = Color.Black;
+            cuiButton2.HoverImageTint = Color.White;
+            cuiButton2.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            cuiButton2.Image = null;
+            cuiButton2.ImageAutoCenter = true;
+            cuiButton2.ImageExpand = new Point(0, 0);
+            cuiButton2.ImageOffset = new Point(0, 0);
+            cuiButton2.Location = new Point(59, 457);
+            cuiButton2.Name = "cuiButton2";
+            cuiButton2.NormalBackground = Color.Olive;
+            cuiButton2.NormalForeColor = Color.Black;
+            cuiButton2.NormalImageTint = Color.White;
+            cuiButton2.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton2.OutlineThickness = 1F;
+            cuiButton2.PressedBackground = Color.WhiteSmoke;
+            cuiButton2.PressedForeColor = Color.FromArgb(32, 32, 32);
+            cuiButton2.PressedImageTint = Color.White;
+            cuiButton2.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton2.Rounding = new Padding(8);
+            cuiButton2.Size = new Size(314, 32);
+            cuiButton2.TabIndex = 1;
+            cuiButton2.TextAlignment = StringAlignment.Center;
+            cuiButton2.TextOffset = new Point(0, 0);
+            // 
+            // cuiButton1
+            // 
+            cuiButton1.CheckButton = false;
+            cuiButton1.Checked = false;
+            cuiButton1.CheckedBackground = Color.FromArgb(255, 106, 0);
+            cuiButton1.CheckedForeColor = Color.White;
+            cuiButton1.CheckedImageTint = Color.Wheat;
+            cuiButton1.CheckedOutline = Color.FromArgb(255, 106, 0);
+            cuiButton1.Content = "Iniciar sesion";
+            cuiButton1.DialogResult = DialogResult.None;
+            cuiButton1.Font = new Font("Garamond", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cuiButton1.ForeColor = Color.Black;
+            cuiButton1.HoverBackground = Color.White;
+            cuiButton1.HoverForeColor = Color.Black;
+            cuiButton1.HoverImageTint = Color.White;
+            cuiButton1.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            cuiButton1.Image = null;
+            cuiButton1.ImageAutoCenter = true;
+            cuiButton1.ImageExpand = new Point(0, 0);
+            cuiButton1.ImageOffset = new Point(0, 0);
+            cuiButton1.Location = new Point(102, 380);
+            cuiButton1.Name = "cuiButton1";
+            cuiButton1.NormalBackground = Color.Orange;
+            cuiButton1.NormalForeColor = Color.Black;
+            cuiButton1.NormalImageTint = Color.White;
+            cuiButton1.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton1.OutlineThickness = 1F;
+            cuiButton1.PressedBackground = Color.WhiteSmoke;
+            cuiButton1.PressedForeColor = Color.FromArgb(32, 32, 32);
+            cuiButton1.PressedImageTint = Color.White;
+            cuiButton1.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton1.Rounding = new Padding(8);
+            cuiButton1.Size = new Size(223, 40);
+            cuiButton1.TabIndex = 0;
+            cuiButton1.TextAlignment = StringAlignment.Center;
+            cuiButton1.TextOffset = new Point(0, 0);
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Chocolate;
+            panel3.Controls.Add(cuiPictureBox1);
+            panel3.Controls.Add(cuiFileDropper1);
+            panel3.Dock = DockStyle.Right;
+            panel3.Location = new Point(450, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(528, 580);
+            panel3.TabIndex = 2;
+            // 
+            // cuiPictureBox1
+            // 
+            cuiPictureBox1.Content = Properties.Resources.browser_735828;
+            cuiPictureBox1.ImageTint = Color.White;
+            cuiPictureBox1.Location = new Point(210, 208);
+            cuiPictureBox1.Margin = new Padding(4, 5, 4, 5);
+            cuiPictureBox1.Name = "cuiPictureBox1";
+            cuiPictureBox1.OutlineThickness = 1F;
+            cuiPictureBox1.PanelOutlineColor = Color.Empty;
+            cuiPictureBox1.Rotation = 0;
+            cuiPictureBox1.Rounding = new Padding(8);
+            cuiPictureBox1.Size = new Size(116, 116);
+            cuiPictureBox1.TabIndex = 2;
+            // 
+            // cuiFileDropper1
+            // 
+            cuiFileDropper1.AllowDrop = true;
+            cuiFileDropper1.BackColor = Color.SaddleBrown;
+            cuiFileDropper1.DashedOutline = true;
+            cuiFileDropper1.DashedOutlineColor = Color.White;
+            cuiFileDropper1.DashLength = 8;
+            cuiFileDropper1.Filter = "";
+            cuiFileDropper1.Font = new Font("Garamond", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cuiFileDropper1.ForeColor = Color.White;
+            cuiFileDropper1.HoverContent = "Release to drop";
+            cuiFileDropper1.HoverForeColor = Color.Black;
+            cuiFileDropper1.HoverUploadForeColor = Color.FromArgb(64, 64, 0);
+            cuiFileDropper1.Image = (Image)resources.GetObject("cuiFileDropper1.Image");
+            cuiFileDropper1.ImagePadding = 2;
+            cuiFileDropper1.ImageSize = new Size(24, 24);
+            cuiFileDropper1.ImageTint = Color.White;
+            cuiFileDropper1.Location = new Point(63, 402);
+            cuiFileDropper1.Multiselect = false;
+            cuiFileDropper1.Name = "cuiFileDropper1";
+            cuiFileDropper1.NormalContent = "Arrastra la foto de tu preferencia";
+            cuiFileDropper1.NormalForeColor = Color.White;
+            cuiFileDropper1.NormalUploadForeColor = Color.Yellow;
+            cuiFileDropper1.OutlineThickness = 1F;
+            cuiFileDropper1.PanelColor = Color.FromArgb(16, 255, 255, 255);
+            cuiFileDropper1.Rounding = new Padding(8);
+            cuiFileDropper1.Size = new Size(399, 112);
+            cuiFileDropper1.TabIndex = 1;
+            cuiFileDropper1.Text = "cuiFileDropper1";
+            cuiFileDropper1.UploadContent = "Click to upload";
+            cuiFileDropper1.UploadWithClick = true;
+            // 
+            // FormInicio
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(978, 580);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "FormInicio";
+            Text = "Incio de sesión";
+            panel2.ResumeLayout(false);
+            cuiGroupBox2.ResumeLayout(false);
+            cuiGroupBox1.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private PictureBox pictureBoxConfig;
+        private ToolTip toolTip1;
+        private Panel panel2;
+        private Panel panel3;
+        private CuoreUI.Controls.cuiButton cuiButton2;
+        private CuoreUI.Controls.cuiButton cuiButton1;
+        private CuoreUI.Controls.cuiGroupBox cuiGroupBox1;
+        private CuoreUI.Controls.cuiLabel cuiLabel1;
+        private CuoreUI.Controls.cuiGroupBox cuiGroupBox2;
+        private CuoreUI.Controls.cuiFileDropper cuiFileDropper1;
+        private CuoreUI.Controls.cuiPictureBox cuiPictureBox2;
+        private CuoreUI.Controls.cuiLabel cuiLabel2;
+        private CuoreUI.Controls.cuiPictureBox cuiPictureBox1;
+        private CuoreUI.Controls.cuiButton cuiButton3;
+        private CuoreUI.Controls.cuiTextBox cuiTextBox2;
+        private CuoreUI.Controls.cuiTextBox cuiTextBox1;
+    }
+}
---
+++ FormApiE-Commerce/Views/PaginaPrincipal.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using FormApiE_Commerce.VerificacionVendedorDTOs;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms;
+using GMap.NET;
+using System.Net.Http.Json;
+using FormApiE_Commerce.Models;
+
+
+
+namespace FormApiE_Commerce
+{
+    public partial class PaginaPrincipal : Form
+    {
+        private void cuiPictureBox1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //public Vendedores vendedor = new Vendedores();
+        //public PaginaPrincipal()
+        //{
+        //    InitializeComponent();
+        //    MostrarFormularioEnTabPage();
+        //}
+        //// Cierra el formulario y abre el formulario FormInicio.
+        //private void btnCerrarSesion_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        var cerrar = MessageBox.Show("Seguro que desea cerrar seción?", "Información", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+        //        if (cerrar == DialogResult.Yes)
+        //        {
+        //            MessageBox.Show("E-Commerce le desea buen dia, vuelva pronto.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //            this.Tag = "FormInicio";
+        //            this.DialogResult = DialogResult.OK;
+        //            this.Close();
+        //            return;
+        //        }
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"Error inesperado al cerrar sesión: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+
+        //}
+
+        //private void btnMenu_Click(object sender, EventArgs e)
+        //{
+        //    flowLayoutPanel1.Visible = true;
+        //    btnMenu.Visible = false;
+        //}
+
+        //private void btnCerrar_Click(object sender, EventArgs e)
+        //{
+        //    flowLayoutPanel1.Visible = false;
+        //    btnMenu.Visible = true;
+        //}
+        //public async void MostrarFormularioEnTabPage()
+        //{
+        //    FormComprar formComprar = new FormComprar();
+        //    formComprar.Dock = DockStyle.Fill;
+        //    tabPageComprar.Controls.Clear();
+        //    tabPageComprar.Controls.Add(formComprar);
+        //    formComprar.Show();
+
+        //    try
+        //    {
+        //      //  int usuarioId = FormInicio.UsuarioId;
+        //        var api = new ApiService();
+
+        //        // Llamar a la API para obtener si es vendedor
+        //      //  var vendedor = await api.ObtenerVendedorPorUsuarioIdAsync(usuarioId);
+
+        //        tabPageVender.Controls.Clear();
+
+        //        if (vendedor != null)
+        //        {
+        //            // Ya es vendedor
+        //            FormVendedorRegistrado interfaz = new FormVendedorRegistrado(); // este es un UserControl
+        //            interfaz.Dock = DockStyle.Fill;
+        //            tabPageVender.Controls.Add(interfaz);
+        //        }
+        //        else
+        //        {
+        //            // No es vendedor
+        //            Vender registro = new Vender(); // este es el formulario de registro
+        //            registro.Dock = DockStyle.Fill;
+        //            tabPageVender.Controls.Add(registro);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("Error al cargar la información del vendedor: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //}
+
+        //public class ApiService
+        //{
+        //    private readonly HttpClient client;
+
+        //    public ApiService()
+        //    {
+        //        client = new HttpClient();
+        //        client.BaseAddress = new Uri("https://localhost:5001/"); // cambia a tu base real
+        //    }
+
+        //    public async Task<VerificacioVendedorDTO> ObtenerVendedorPorUsuarioIdAsync(int usuarioId)
+        //    {
+        //        var response = await client.GetAsync($"api/vendedores/usuario/{usuarioId}");
+
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            return await response.Content.ReadFromJsonAsync<VerificacioVendedorDTO>();
+        //        }
+
+        //        return null;
+        //    }
+        //}
+
+
+        //private void pictureBox2_DoubleClick(object sender, EventArgs e)
+        //{
+        //    Chatbot chatbot = new Chatbot();
+        //    chatbot.Show();
+        //}
+    }
+}
