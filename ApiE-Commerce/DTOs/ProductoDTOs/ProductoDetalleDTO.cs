2b3646ae2d304fff8cfb20ca67c5ae91e2bf9047
rafael.espinoza484@std.uni.edu.ni <rafael.espinoza484@std.uni.edu.ni>
2025-11-28 00:55:20 -0600

    daoproducto

---
+++ ApiE-Commerce/DTOs/ProductoDTOs/ProductoDetalleDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace ApiProyectoDeCursoE_Commerce.DTOs.ProductoDTOs
+{
+    public class ProductoDetalleDTO
+    {
+        public int IdProducto { get; set; }
+        public required string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public required string Descripcion { get; set; }
+        public int StockDisponible { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+
+        // Datos JOIN
+        public required string NombreCategoria { get; set; }
+        public required string NombreEstado { get; set; }
+
+        // Información del Vendedor
+        public required string NombreNegocio { get; set; }
+        public required string LogoNegocioUrl { get; set; }
+
+        // Todas las imágenes
+        public required List<string> UrlImagenes { get; set; }
+    }
+}
