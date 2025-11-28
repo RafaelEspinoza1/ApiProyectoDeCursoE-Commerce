42cd96bd882b55e1ae17c6c32454d55d4d1ed87f
Lester Antonio Rodríguez Tenorio <lestertenorio30@gmail.com>
2025-11-28 00:01:59 -0600

    Se añadieron formularios para funciones financieras.

---
+++ FormApiE-Commerce/UsersControls/AnalisisNegocio.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace FormApiE_Commerce.UsersControls
+{
+    partial class AnalisisNegocio
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSistemaDupont = new CuoreUI.Controls.cuiButton();
+            BtnRazonesFinancieras = new CuoreUI.Controls.cuiButton();
+            SuspendLayout();
+            // 
+            // btnSistemaDupont
+            // 
+            btnSistemaDupont.CheckButton = false;
+            btnSistemaDupont.Checked = false;
+            btnSistemaDupont.CheckedBackground = Color.FromArgb(255, 106, 0);
+            btnSistemaDupont.CheckedForeColor = Color.White;
+            btnSistemaDupont.CheckedImageTint = Color.White;
+            btnSistemaDupont.CheckedOutline = Color.FromArgb(255, 106, 0);
+            btnSistemaDupont.Content = "Sistema Dupont";
+            btnSistemaDupont.DialogResult = DialogResult.None;
+            btnSistemaDupont.Font = new Font("Garamond", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSistemaDupont.ForeColor = Color.Black;
+            btnSistemaDupont.HoverBackground = Color.White;
+            btnSistemaDupont.HoverForeColor = Color.Black;
+            btnSistemaDupont.HoverImageTint = Color.White;
+            btnSistemaDupont.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            btnSistemaDupont.Image = null;
+            btnSistemaDupont.ImageAutoCenter = true;
+            btnSistemaDupont.ImageExpand = new Point(0, 0);
+            btnSistemaDupont.ImageOffset = new Point(0, 0);
+            btnSistemaDupont.Location = new Point(321, 144);
+            btnSistemaDupont.Margin = new Padding(4);
+            btnSistemaDupont.Name = "btnSistemaDupont";
+            btnSistemaDupont.NormalBackground = Color.White;
+            btnSistemaDupont.NormalForeColor = Color.Black;
+            btnSistemaDupont.NormalImageTint = Color.White;
+            btnSistemaDupont.NormalOutline = Color.Black;
+            btnSistemaDupont.OutlineThickness = 1F;
+            btnSistemaDupont.PressedBackground = Color.WhiteSmoke;
+            btnSistemaDupont.PressedForeColor = Color.FromArgb(32, 32, 32);
+            btnSistemaDupont.PressedImageTint = Color.White;
+            btnSistemaDupont.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            btnSistemaDupont.Rounding = new Padding(8);
+            btnSistemaDupont.Size = new Size(239, 70);
+            btnSistemaDupont.TabIndex = 6;
+            btnSistemaDupont.TextAlignment = StringAlignment.Center;
+            btnSistemaDupont.TextOffset = new Point(0, 0);
+            // 
+            // BtnRazonesFinancieras
+            // 
+            BtnRazonesFinancieras.CheckButton = false;
+            BtnRazonesFinancieras.Checked = false;
+            BtnRazonesFinancieras.CheckedBackground = Color.FromArgb(255, 106, 0);
+            BtnRazonesFinancieras.CheckedForeColor = Color.White;
+            BtnRazonesFinancieras.CheckedImageTint = Color.White;
+            BtnRazonesFinancieras.CheckedOutline = Color.FromArgb(255, 106, 0);
+            BtnRazonesFinancieras.Content = "Razones Financieras";
+            BtnRazonesFinancieras.DialogResult = DialogResult.None;
+            BtnRazonesFinancieras.Font = new Font("Garamond", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BtnRazonesFinancieras.ForeColor = Color.Black;
+            BtnRazonesFinancieras.HoverBackground = Color.White;
+            BtnRazonesFinancieras.HoverForeColor = Color.Black;
+            BtnRazonesFinancieras.HoverImageTint = Color.White;
+            BtnRazonesFinancieras.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            BtnRazonesFinancieras.Image = null;
+            BtnRazonesFinancieras.ImageAutoCenter = true;
+            BtnRazonesFinancieras.ImageExpand = new Point(0, 0);
+            BtnRazonesFinancieras.ImageOffset = new Point(0, 0);
+            BtnRazonesFinancieras.Location = new Point(44, 144);
+            BtnRazonesFinancieras.Margin = new Padding(4);
+            BtnRazonesFinancieras.Name = "BtnRazonesFinancieras";
+            BtnRazonesFinancieras.NormalBackground = Color.White;
+            BtnRazonesFinancieras.NormalForeColor = Color.Black;
+            BtnRazonesFinancieras.NormalImageTint = Color.White;
+            BtnRazonesFinancieras.NormalOutline = Color.Black;
+            BtnRazonesFinancieras.OutlineThickness = 1F;
+            BtnRazonesFinancieras.PressedBackground = Color.WhiteSmoke;
+            BtnRazonesFinancieras.PressedForeColor = Color.FromArgb(32, 32, 32);
+            BtnRazonesFinancieras.PressedImageTint = Color.White;
+            BtnRazonesFinancieras.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            BtnRazonesFinancieras.Rounding = new Padding(8);
+            BtnRazonesFinancieras.Size = new Size(239, 70);
+            BtnRazonesFinancieras.TabIndex = 5;
+            BtnRazonesFinancieras.TextAlignment = StringAlignment.Center;
+            BtnRazonesFinancieras.TextOffset = new Point(0, 0);
+            // 
+            // AnalisisNegocio
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnSistemaDupont);
+            Controls.Add(BtnRazonesFinancieras);
+            Name = "AnalisisNegocio";
+            Size = new Size(1486, 1065);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private CuoreUI.Controls.cuiButton btnSistemaDupont;
+        private CuoreUI.Controls.cuiButton BtnRazonesFinancieras;
+    }
+}
